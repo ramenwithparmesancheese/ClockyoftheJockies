b088dd37aaf52d0588cc3199b42f1b05a46eff6e
Nectar <74465738+ramenwithparmesancheese@users.noreply.github.com>
2022-10-19 11:59:56 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from asyncore import read
+from lib2to3.pgen2.token import STRING
+from tkinter import *
+from tkinter.ttk import *
+
+from time import strftime
+
+root = Tk()
+root.title("Clock")
+print("Pick a color")
+read = input() 
+
+def time():
+    string = strftime('%H:%M:%S %p')
+    label.config(text=string)
+    label.after(1000, time)
+
+label = Label(root, font=("ds-digital", 80), background = "black", foreground = read)
+label.pack(anchor='center')
+time()
+
+mainloop()
